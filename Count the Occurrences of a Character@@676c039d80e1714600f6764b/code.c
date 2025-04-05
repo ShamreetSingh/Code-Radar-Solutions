b84d2cb0f1a5d35419a8f18fef647b2396d1fb17
SHAMREET SINGH <shamreetsingh7777@gmail.com>
2025-04-05 22:12:25 +0530

    Solution to Count the Occurrences of a Character

--- Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
+++ Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
@@ -1,41 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-
-int are_anagrams(char *str1, char *str2) {
-    int freq[256] = {0};  
-
-    if (strlen(str1) != strlen(str2)) {
-        return 0;
-    }
-
-    for (int i = 0; str1[i] != '\0'; i++) {
-        freq[(unsigned char)str1[i]]++;
-        freq[(unsigned char)str2[i]]--;
-    }
-
-    for (int i = 0; i < 256; i++) {
-        if (freq[i] != 0) {
-            return 0;
+int main() {
+    char str[100];
+    char charToCount;
+    int count = 0;
+    printf("Enter the string: ");
+    fgets(str, sizeof(str), stdin);
+    printf("Enter the character to count: ");
+    scanf(" %c", &charToCount);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == charToCount) {
+            count++;
         }
     }
-
-    return 1;
-}
-
-int main() {
-    char str1[100], str2[100];
-
-    fgets(str1, sizeof(str1), stdin);
-    fgets(str2, sizeof(str2), stdin);
-
-    str1[strcspn(str1, "\n")] = '\0';
-    str2[strcspn(str2, "\n")] = '\0';
-
-    if (are_anagrams(str1, str2)) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
-
+    printf("%d\n", count);
     return 0;
 }
